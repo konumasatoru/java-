bbc2e38400081dc5ceb28ac21a98601a7f7ee79b
onumasatoru <onumasatoru@localhost>
2024-02-16 23:40:56 +0900

    課題再提出

--- HelloWorld_FamilynameFirstname/src/curriculum_B/Qes6.java
+++ HelloWorld_FamilynameFirstname/src/curriculum_B/Qes6.java
@@ -1,17 +1,21 @@
 package curriculum_B;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Qes6 {
 
 	public static void main(String[] args) {
-        String input = "パソコン、冷蔵庫、扇風機、洗濯機、加湿器、テレビ、ディスプレイ、その他商品";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("");
+        String input = scanner.nextLine();
         String[] products = input.split("、");
-        int tvCount = new Random().nextInt(12);
+        Random random = new Random();
+        int tvCount = random.nextInt(12);
         
         // [詳細]出力を生成
         for (String product : products) {
-            int count = new Random().nextInt(12);
+            int count = random.nextInt(12);
             String output;
             
             switch (product) {
@@ -20,13 +24,13 @@ public class Qes6 {
                 case "扇風機":
                 case "洗濯機":
                 case "加湿器":
-                    output = product + "の残り台数は" + count + "台です";
+                    output = product + "の残り台数は" + count + "台です\n";
                     break;
                 case "テレビ":
-                	output = "テレビの残り台数は" + tvCount + "台です";
+                	output = "テレビの残り台数は" + tvCount + "台です\n";
                     break;
                 case "ディスプレイ":
-                    output = "ディスプレイの残りの台数は" + (11 - tvCount ) + "台です";
+                    output = "ディスプレイの残りの台数は" + (11 - tvCount ) + "台です\n";
                     break;
                 default:
                     output = "『" + product + "』は指定の商品ではありません";
